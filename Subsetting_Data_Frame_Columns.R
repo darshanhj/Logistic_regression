45bae241dc5185641dec6f6da7042212b558dc3e
DarshanHJ <42734022+darshanhj@users.noreply.github.com>
2019-02-20 01:41:36 +0530

    Add files via upload

---
+++ Subsetting_Data_Frame_Columns.R
@@ -0,0 +1,12 @@
+my_data <- iris
+library("tibble")
+my_data <- as_data_frame(my_data)
+my_data
+
+install.packages("dplyr", dependencies =TRUE)
+
+
+install.packages("dplyr",repos="http://cran.rstudio.com/", dependencies=TRUE, type="source")
+library("dplyr")
+
+
